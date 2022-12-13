5d5ab7ddef5c050b5e65773d55b1ebd578223fd6
Maithê <augustocezar607@gmail.com>
2022-12-12 22:19:44 -0300

    Parte final

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.myapplication.ui.main.SectionsPagerAdapter;
 import com.example.myapplication.databinding.ActivityMainBinding;
@@ -20,9 +22,22 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+    private Button botao;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        botao = findViewById(R.id.button);
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Sobrenos intent = new Sobrenos(MainActivity.this,Sobrenos.class);
+                startActivity(Sobrenos);
+            }
+        }ViewO);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -42,4 +57,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-}
+    private Button botãosobrenos;
+
+    @Override
+    public <T extends View> T findViewById(int id) {
+        return super.findViewById(id);
+
+        botãosobrenos = findViewById(R.id.button);
+
+        botãosobrenos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,SobreNos.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    private Button botaoMinhaConta;
+
+    @Override
+    public <T extends View> T findViewById(int id) {
+        return super.findViewById(id);
+
+        botaoMinhaConta = findViewById(R.id.button);
+
+        botaoMinhaConta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,Perfil2.class);
+                startActivity(intent);
+            }
+        });
+
+    }
--- app/src/main/java/com/example/myapplication/Perfil2.java
+++ app/src/main/java/com/example/myapplication/Perfil2.java
@@ -7,47 +7,52 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class SobreNos extends AppCompatActivity {
+public class Perfil2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sobre_nos);
+        setContentView(R.layout.activity_perfil2);
     }
 
-    private Button botãoMinhaConta;
-
-    @Override
-    public <T extends View> T findViewById(int id) {
-        return super.findViewById(id);
-
-        botãoMinhaConta = findViewById(R.id.button);
-
-        botãoMinhaConta.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(SobreNos.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
-    }
-
-    private Button botaoHome;
-
-    @Override
-    public <T extends View> T findViewById(int id) {
-        return super.findViewById(id);
-
-        botaoHome = findViewById(R.id.button);
-
-        botaoHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(SobreNos.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
 }
+    private Button botãoHome;
+
+    @Override
+    public <T extends View> T findViewById(int id) {
+        return super.findViewById(id);
+
+        botãoHome = findViewById(R.id.button);
+
+        botãoHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, Perfil2.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+    private Button botaoSobreNos;
+
+    @Override
+    public <T extends View> T findViewById(int id) {
+        return super.findViewById(id);
+
+        botaoSobreNos = findViewById(R.id.button);
+
+        botaoSobreNos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, Perfil2.class);
+                startActivity(intent);
+            }
+
+        });
+
+    }
+}
+
